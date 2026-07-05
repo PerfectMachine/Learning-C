f6555ecab5deab53fbcafb0db5d79b7ce293ddf3
PerfectMachine <340890@edu.itmo.ru>
2026-07-05 22:21:00 +0300

    New project

---
+++ project2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    float radius;
+    float area;
+    float surfaceArea;
+    float volume;
+    float pi = 3.141592;
+
+    printf("Enter desired radius: ");
+    scanf("%f", &radius);
+
+    printf("aaa");
+    area = pi * (pow(radius,2));
+    surfaceArea = 4 * area;
+    volume = (4 / 3) * pi * (pow(radius,3));
+    
+    printf("Chosen radius: %f\n", radius);
+    printf("Area: %f\n", area);
+    printf("Surface Area: %f\n", surfaceArea);
+    printf("Volume: %f\n", volume);
+
+    return 0;
+}
