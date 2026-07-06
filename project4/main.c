33c38a91e00355e5bfa38415e1b0c573dcdac236
PerfectMachine <340890@edu.itmo.ru>
2026-07-06 16:28:41 +0300

    Simple calculator project

---
+++ project4/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    float num1;
+    float num2;
+    char operator;
+
+    printf("Calculator app is on!\n");
+    printf("Enter the first number: ");
+    scanf(" %f",&num1);
+    printf("Choose operation (+ - * /): ");
+    scanf(" %c", &operator);
+    printf("Enter the second number: ");
+    scanf(" %f",&num2);
+
+    switch (operator)
+    {
+    case '+':
+        printf("Your result is %.2f", num1+num2);
+        break;
+
+    case '-':
+        printf("Your result is %.2f", num1-num2);
+        break;
+
+    case '*':
+        printf("Your result is %.2f", num1*num2);
+        break;
+    
+    case '/':
+        printf("Your result is %.2f", num1/num2);
+        break;
+    default:
+        printf("Invalid input! Make sure to use only defined operators (+ - * /)");
+        break;
+    }
+
+    return 0;
+}
